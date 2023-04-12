60619bb8d3ae36f300c2c0d8a1ef720f58a7d507
sonovrik <dmc2001@mail.ru>
2023-04-12 11:35:58 +0300

    Added tg core function for event handling

--- core/telegram/tgCore.go
+++ core/telegram/tgCore.go
@@ -1,6 +1,10 @@
 package telegram
 
-import "PersonalPlanner/events"
+import (
+	"PersonalPlanner/events"
+	"log"
+	"time"
+)
 
 type TgCore struct {
 	receiver  events.Receiver
@@ -20,6 +24,38 @@ func New(receiver events.Receiver, processor events.Processor, sender events.Sen
 
 func (c TgCore) Start() error {
 	for {
+		newEvents, err := c.receiver.Receive(c.batchSize)
+		if err != nil {
+			log.Printf("Error in tgCore recieve: %s", err)
 
+			continue
+		}
+
+		if len(newEvents) == 0 {
+			time.Sleep(2 * time.Second)
+
+			continue
+		}
+
+		if err = c.handleEvents(newEvents); err != nil {
+			log.Print(err)
+
+			continue
+		}
 	}
+}
+
+// TODO handle errors and parallels
+func (c TgCore) handleEvents(events []events.Event) error {
+	for _, event := range events {
+		log.Printf("got new event: %s", event.Text)
+
+		if err := c.processor.Process(event); err != nil {
+			log.Printf("can't handle event: %s", err)
+
+			continue
+		}
+	}
+
+	return nil
 }
--- events/type.go
+++ events/type.go
@@ -1,13 +1,26 @@
 package events
 
 type Receiver interface {
-	Receive(limit int) error
+	Receive(limit int) ([]Event, error)
 }
 
 type Processor interface {
-	Process() error
+	Process(e Event) error
 }
 
 type Sender interface {
 	Send() error
+}
+
+type Type int
+
+const (
+	Unknown Type = iota
+	Message
+)
+
+type Event struct {
+	Type Type
+	Text string
+	Meta interface{}
 }
